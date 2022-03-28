2cdb5306cd7aebe6c8cdf1bf39e632598d540da2
Henry Leach <henryleach@github.com>
2022-03-28 22:29:38 +0200

    Changed both large and small fonts to U8g2 fonts.

--- epaper_clock/epaper_clock.ino
+++ epaper_clock/epaper_clock.ino
@@ -1,21 +1,18 @@
-#include <U8g2_for_Adafruit_GFX.h>
-#include <u8g2_fonts.h>
-
 // Simple Clock with date on epaper screen with Arduino
 // www.henryleach.com
 
-#include <DS3232RTC.h>      // https://github.com/JChristensen/DS3232RTC
-// select the display class and display driver class in the following file (new style):
-
+#include <DS3232RTC.h> // https://github.com/JChristensen/DS3232RTC
+#include <LowPower.h>  // https://github.com/rocketscream/Low-Power
+#include <U8g2_for_Adafruit_GFX.h>
+#include <u8g2_fonts.h>
 #include <GxEPD2_BW.h> // including both doesn't use more code or ram
 #include <GxEPD2_3C.h> // including both doesn't use more code or ram
-#include <Fonts/FreeSansBold12pt7b.h>
+// select the display class and display driver class in the following file (new style):
 #include "GxEPD2_display_selection_new_style.h"
-#include <LowPower.h> // https://github.com/rocketscream/Low-Power
 
 U8G2_FOR_ADAFRUIT_GFX u8g2Fonts;  // font constructor
-
 DS3232RTC myRTC;  // create the RTC object
+
 const uint8_t wakeUpPin(2); // connect Arduino pin D2 to RTC's SQW pin.
 char dateString[18]; //longest is "31 September 2000" plus terminator.
 char timeString[6];
@@ -37,7 +34,6 @@ void setup()
 
   u8g2Fonts.begin(display); //connect the u8g2
 
-
   // initialize the alarms to known values, clear the alarm flags, clear the alarm interrupt flags
   myRTC.setAlarm(DS3232RTC::ALM1_MATCH_DATE, 0, 0, 0, 1);
   myRTC.setAlarm(DS3232RTC::ALM2_MATCH_DATE, 0, 0, 0, 1);
@@ -57,37 +53,36 @@ void setup()
 }
 
 // Displays the date in the bottom half of the screen
-// does a complete screen refresh
+// and does a complete screen refresh
 void displayDate()
 {
   display.init();
 
-  //Now print the date in smaller text below.
-  //Create the date string
-  //write the date in first
   itoa(day(t), dateString, 10); //base 10
   itoa(year(t), yearString, 10);
+  
   strcat(dateString, " ");
   strcat(dateString, monthStr(month(t)));
   strcat(dateString, " ");
   strcat(dateString, yearString);
   Serial.println(dateString);
-  display.setRotation(1); //0 is 'portrait'
-  display.setFont(&FreeSansBold12pt7b); //This almost fills the screen with "Wednesday"
-  display.setTextColor(GxEPD_BLACK);
-  int16_t tbx, tby;
-  uint16_t tbw, tbh;
-  display.getTextBounds(dateString, 0, 0, &tbx, &tby, &tbw, &tbh);
-  uint16_t x = ((display.width() - tbw) / 2) - tbx;  // centres the text
-  uint16_t y = 100; //bottom
+  
+  display.setRotation(3); //0 is 'portrait'
+
+  u8g2Fonts.setForegroundColor(GxEPD_BLACK);
+  u8g2Fonts.setBackgroundColor(GxEPD_WHITE);
+  u8g2Fonts.setFont(u8g2_font_logisoso20_tr);
+  
+  uint16_t x = ((display.width() - u8g2Fonts.getUTF8Width(dateString)) / 2);  // centres the text
+  uint16_t y = 110; //bottom
   // covers bottom half
   display.setFullWindow();
   display.firstPage();
-  do // Print the upper part of the screen
+  do // update the whole screen
   {
-    display.fillScreen(GxEPD_WHITE);
-    display.setCursor(x, y);
-    display.print(dateString);
+    //display.fillScreen(GxEPD_WHITE);
+    u8g2Fonts.setCursor(x, y);
+    u8g2Fonts.print(dateString);
   }
   while (display.nextPage());
   display.hibernate();
@@ -124,11 +119,11 @@ void displayTime()
   
   uint16_t x = ((display.width() - u8g2Fonts.getUTF8Width(timeString)) / 2);  // centres the text
   Serial.println(x);
-  uint16_t y = 60; //top half
+  uint16_t y = 64; //top half
 
   display.setPartialWindow(0, 0, display.width(), display.height() / 2);
   display.firstPage();
-  do // Print the upper part of the screen
+  do // Update the upper part of the screen
   {
     u8g2Fonts.setCursor(x, y);
     u8g2Fonts.print(timeString);
@@ -155,15 +150,15 @@ void loop()
 
   setAlarm();
 
-  //Allow wake up pin to trigger on interrupt low.
+  // Allow wake up pin to trigger on interrupt low.
   attachInterrupt(digitalPinToInterrupt(2), alarmIsr, FALLING);
 
-  //power down
+  // Power down
   LowPower.powerDown(SLEEP_FOREVER, ADC_OFF, BOD_OFF);
 
-  //Wakes up here!
+  // Wakes up here!
 
-  //disable external pin interrupt on wake up pin.
+  // Disable external pin interrupt on wake up pin.
   detachInterrupt(digitalPinToInterrupt(2));
   Serial.println("Woke Up!");
   if (myRTC.alarm(DS3232RTC::ALARM_1)) {
@@ -173,7 +168,7 @@ void loop()
 }
 
 void setAlarm() {
-  //setAlarm(alarmType, seconds, minutes, hours, daydate)
+  // setAlarm(alarmType, seconds, minutes, hours, daydate)
   myRTC.setAlarm(DS3232RTC::ALM1_MATCH_SECONDS, 0, 0, 0, 1); // starts on next whole minute
   myRTC.alarm(DS3232RTC::ALARM_1);
   myRTC.alarmInterrupt(DS3232RTC::ALARM_1, true);
@@ -181,5 +176,6 @@ void setAlarm() {
 
 void alarmIsr()
 {
+  // Need a function for the interrupt, but doesn't have to do anything.
   Serial.println("Interrupt Triggered");
 }
